73e68b854a521518c0bff2622fc1a7e94043b464
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 15:24:34 +0300

    feat: add Organization schema

---
+++ src/organization/entities/organization.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { User } from 'src/user/entities/user.entity';
+
+@Schema({ timestamps: true}) // Set strictPopulate here
+export class Organization extends Document {
+
+  @Prop({ required: true })
+  name: string;
+
+
+
+  @Prop({ required: true, unique: true })
+  description: string;
+
+
+  
+ // Ensure that organization_members references User schema
+ @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: User.name }], default: [] })
+ organization_members: User[];
+}
+
+export const organizationSchema = SchemaFactory.createForClass(Organization);
