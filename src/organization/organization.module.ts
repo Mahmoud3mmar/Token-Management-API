1973fd3203b8e5aa3c367cdcb5006f968bd6e65f
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 15:41:08 +0300

    feat: add Organization Module

---
+++ src/organization/organization.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { OrganizationService } from './organization.service';
+import { OrganizationController } from './organization.controller';
+import { Organization, organizationSchema } from './entities/organization.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
+import { User, UserSchema } from 'src/user/entities/user.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Organization.name, schema: organizationSchema },{ name: User.name, schema: UserSchema }]),
+    UserModule, // Include UserModule here
+  ],
+  controllers: [OrganizationController],
+  providers: [OrganizationService],
+})
+export class OrganizationModule {}
