b1c5c24cda6c2ccf033720eedecfcabdad2216cc
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 15:30:01 +0300

    feat: add getOrganizationById service to retrieve organization details by ID.

--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -4,6 +4,7 @@ import { Organization } from './entities/organization.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserService } from '../user/user.service';
+import { GetOrganizationDto } from './dto/get-organization.dto';
 
 
 @Injectable()
@@ -30,7 +31,31 @@ export class OrganizationService {
     return await newOrganization.save();
   }
   
+  async getOrganizationById(organizationId: string): Promise<GetOrganizationDto> {
+    // Find the organization by ID
+    const organization = await this.organizationModel
+      .findById(organizationId)
+      .populate('organization_members', 'name email access_level') // Populate members with name and email
+      .exec();
 
+    if (!organization) {
+      throw new NotFoundException('Organization not found');
+    }
+
+    // Map organization members to the desired response format
+    const organizationMembers = organization.organization_members.map(member => ({
+      name: member.name,
+      email: member.email,
+      access_level: member.access_level, // Ensure this field exists in the User model
+    }));
+
+    return {
+      organization_id: organization.id,
+      name: organization.name,
+      description: organization.description,
+      organization_members: organizationMembers,
+    };
+  }
   
   
 }
