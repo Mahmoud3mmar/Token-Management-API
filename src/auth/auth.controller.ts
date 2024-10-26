1fcdddb8695d18296253e0ba29719899b500a179
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 19:14:12 +0300

    feat : add swagger documentation decorators on routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,9 @@ import { AuthService } from './auth.service';
 
 import {
 
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { SignUpAuthDto } from './dto/signup.auth.dto';
@@ -22,6 +25,15 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: 'User Signup' }) // Operation summary
+  @ApiResponse({
+    status: 201,
+    description: 'User successfully registered.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input data.',
+  })
    async signUp(@Body() signUpAuthDto: SignUpAuthDto) {
     return this.authService.signUp(signUpAuthDto);
   }
@@ -29,7 +41,15 @@ export class AuthController {
 
 
   @Post('signin')
-
+  @ApiOperation({ summary: 'User Signin' }) // Operation summary
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully signed in.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid credentials.',
+  })
   async signIn(@Body() loginAuthDto: LoginAuthDto) {
     return this.authService.signIn(loginAuthDto);
   }
@@ -37,7 +57,16 @@ export class AuthController {
 
   
   @Post('refresh/token')
- 
+  @ApiOperation({ summary: 'Refresh Token' }) // Operation summary
+  @ApiQuery({ name: 'refreshToken', required: true, description: 'The refresh token to be used.' }) // Query parameter
+  @ApiResponse({
+    status: 200,
+    description: 'Token successfully refreshed.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid or expired refresh token.',
+  })
   async refreshToken(@Query('refreshToken') refreshToken: string) {
     return this.authService.refreshToken(refreshToken);
   }
