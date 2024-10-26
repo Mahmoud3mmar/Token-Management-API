0786cf533eb585c7dbd6f49b9a3db30244b6c29c
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 15:03:09 +0300

    feat: add signin service for user registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,10 +12,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose'; // Import ObjectId here
 
 import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 
 import { User } from '../user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpAuthDto } from './dto/signup.auth.dto';
+import { LoginAuthDto } from './dto/login.auth.dto';
 
 import { MailService } from 'src/global services/Email.Service';
 import { AccessLevel } from 'src/user/common utils/AccessLevel.enum';
@@ -26,7 +28,7 @@ export class AuthService {
 
     private readonly jwtService: JwtService,
     private readonly mailService: MailService,
-  ) {}
+    ) {}
 
   async signUp(signUpAuthDto: SignUpAuthDto): Promise<{ message: string }> {
     const { name, email, password, confirmPassword } = signUpAuthDto;
@@ -78,5 +80,96 @@ export class AuthService {
       throw new InternalServerErrorException('Failed to sign up user', error);
     }
   }
+  async signIn(
+    loginAuthDto: LoginAuthDto,
+  ): Promise<{ access_token: string; refresh_token: string; message: string }> {
+    const { email, password } = loginAuthDto;
 
+    try {
+      // Validate the user credentials
+      const user = await this.validateUser(email, password);
+
+      if (!user) {
+        throw new UnauthorizedException('Invalid email or password');
+      }
+
+
+      // Invalidate the previous refresh token
+      await this.invalidateOldRefreshToken(user._id.toString());
+
+      // Generate new access and refresh tokens
+      const tokens = await this.generateTokens(
+        user._id.toString(),
+        user.email,
+        user.access_level,
+      );
+
+      // Update the user's record with the new refresh token
+      await this.updateRefreshToken(user._id.toString(), tokens.refreshToken);
+
+      return {
+        access_token: tokens.accessToken,
+        refresh_token: tokens.refreshToken,
+        message: 'Successfully signed in',
+      };
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error; // Re-throw known authentication errors
+      }
+
+      // Log unexpected errors for diagnostics
+      console.error('Sign in error:', error);
+
+      // Handle unexpected errors
+      throw new InternalServerErrorException('Failed to sign in user');
+    }
+  }
+
+
+  
+  async invalidateOldRefreshToken(userId: string): Promise<void> {
+    // Fetch the user and check if there is an existing refresh token
+    const user = await this.userModel.findById(userId);
+
+    if (user && user.refreshToken) {
+      // Invalidate the old refresh token
+      user.refreshToken = null;
+      await user.save();
+    }
+  }
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userModel.findOne({ email });
+
+    // Validate user existence and password
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+
+    throw new UnauthorizedException('Please check your login credentials');
+  }
+
+
+  private async generateTokens(userId: string, email: string, role: string) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        { sub: userId, email, role },
+        { secret: process.env.JWT_ACCESS_SECRET, expiresIn: '5h' },
+      ),
+      this.jwtService.signAsync(
+        { sub: userId, email, role },
+        { secret: process.env.JWT_REFRESH_SECRET, expiresIn: '7d' },
+      ),
+    ]);
+
+    return { accessToken, refreshToken };
+  }
+
+  async updateRefreshToken(
+    userId: string,
+    refreshToken: string,
+  ): Promise<void> {
+    // Update the user's refresh token in the database
+    await this.userModel.findByIdAndUpdate(userId, { refreshToken });
+  }
 }
