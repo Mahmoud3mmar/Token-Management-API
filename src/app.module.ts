006a825731fd9e4cc95af56a2c919207a5365575
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-26 16:38:15 +0300

    feat(module): Integrate AuthModule,UserModule,OrganizationModule, RedisModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { OrganizationModule } from './organization/organization.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [
+  imports: [AuthModule, UserModule, OrganizationModule,
     ConfigModule.forRoot({
     isGlobal: true,
   }),
   MongooseModule.forRoot(process.env.MONGODB_URI, {
     family: 4,
 
-  })
+  }) ,RedisModule
 ],
   controllers: [AppController],
   providers: [AppService],
